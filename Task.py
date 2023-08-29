def7770416b84a07c89c3f81b5cedcac0f90e991
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2023-08-29 23:25:19 +0200

    task file added

---
+++ Task.py
@@ -0,0 +1,145 @@
+# # Task 1 - fix the FizzBuzz
+
+# three_mul = 'fizz'
+# five_mul = 'buzz'
+# num1 = 3
+# num2 = 5
+# max_num = 100
+   
+# for i in range(1, max_num + 1):
+    
+#     if i%num1 == 0 and i%num2 == 0:
+#         print(i, three_mul+five_mul)
+#     elif i%num2 == 0:
+#         print(i, five_mul)
+#     elif i%num1 == 0:
+#         print(i,three_mul)
+
+
+# # Task 2 - summing integers
+
+# n = 5
+# number = 1
+# sum = 0
+# while number < n + 1:
+#     sum = sum + number
+#     number = number + 1
+# print("Sum =", sum)
+
+
+# # Task 3 - summing integers with range()
+
+# n = 5
+# sum = 0
+# for num in range(1,n+1):
+#     sum += num
+# print("Sum =", sum)
+
+
+# Task 4 - printing in loops
+
+# for x in range(3):
+#     print(x)
+
+
+# for j in range(5):
+#     print(f"This is loop number {j}")
+
+# x = 10
+
+# while x > 0:
+#     print(x)
+#     x -= 1
+    
+# x = 10
+
+# countdown = 5
+# while countdown:
+#     print(f"{countdown}")
+#     countdown -= 1
+# else:
+#     print("Start!")
+
+
+# Task 5 - summing three integers
+
+# x = input("First number: ")
+# y = input("Second number: ")
+# z = input("Third number: ")
+
+# if x == y or y == z:
+#     result = 0
+
+# elif x == z:
+#     result = 0
+# else:
+#     result = x + y + z
+
+# print("Calculated sum is ", result)
+
+
+#     if user_guess == number_to_guess:
+#         print("Well guessed!")
+#         break 
+#     else:
+#         print("Try again!")
+
+
+# x = int(input("First number: "))
+# y = int(input("Second number: "))
+
+# result = x + y
+
+# if result > 15 and result < 20:
+#     result = 20
+#     print("Calculated sum is ", result)
+
+# else:
+#     print("Calculated sum is ", result)
+
+
+# a = input("First value: ")
+# b = input("Second value: ")
+
+# print("Before swapping: a =", a, " b =", b)
+
+# temp = a
+# a = b
+# b = temp
+
+# print("After swapping: a =", a, " b =" b)
+
+
+# x = float(input("First number: "))
+# y = float(input("Second number: "))
+# z = float(input("Third number: "))
+
+# print("The maximum value is ", max(x, y ,z))
+# print("The minimum value is ", min(x, y ,z))
+
+
+# x = input("Type your value: ")
+
+# if x == "0":
+#     x = False
+# elif x == "1":
+#     x = True
+# else:
+#     pass
+
+
+# print("Your entered value is now ", x)
+
+
+
+# x = int(input("First number: "))
+# y = int(input("Second number: "))
+
+# if x % y == 0:
+#     print("First number is divisible by second number, result =", x / y)
+# elif y % x == 0:
+#     print("Second number is divisible by first number, result =", y / x)
+# else:
+#     print("Numbers are non-divisible!")
+
+
